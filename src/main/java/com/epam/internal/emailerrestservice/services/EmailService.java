4a20d86b099894ff54d98245ce47520f747863aa
Parthoprotim Mukherjee <parthoprotim_mukherjee@epam.com>
2019-12-05 15:09:14 +0530

    Test webhook

--- src/main/java/com/epam/internal/emailerrestservice/services/EmailService.java
+++ src/main/java/com/epam/internal/emailerrestservice/services/EmailService.java
@@ -2,8 +2,10 @@ package com.epam.internal.emailerrestservice.services;
 
 import javax.mail.MessagingException;
 
-import com.epam.internal.emailerrestservice.entity.Email;
+import org.springframework.stereotype.Service;
 
+import com.epam.internal.emailerrestservice.entity.Email;
+@Service
 public interface EmailService {
 
 	public String sendEmail(Email email) throws MessagingException;
